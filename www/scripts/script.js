8fd45221582d9b951ce03da4db3594b84bd3aea0
Kirill Demidov <kademidov@gmail.com>
2021-10-28 15:22:32 +0300

    Добавлена страница вопросов и ответов. jquery + мобильное меню

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,1 +1,38 @@
-console.log('script work!');
+$(document).ready(function(){
+
+  // let isOpen = false;
+
+  $('.js-burger').on('click', function(){
+
+    $('.js-main-nav').toggleClass('main-nav-open');
+
+    // if (isOpen) {
+    //   $('.js-main-nav').removeClass('main-nav-open');
+    //   isOpen = false;
+    //   return;
+    // }
+
+    // $('.js-main-nav').addClass('main-nav-open');
+    // isOpen = true;
+
+  });
+
+
+
+  let prevBtn;
+
+  $('.js-accordion-btn').on('click', function() {
+    console.log($(this));
+
+
+    if (prevBtn === $(this)[0]) {
+      $(this).next().slideToggle();
+      return;
+    }
+
+    $('.js-accordion-btn').next().slideUp();
+    $(this).next().slideDown();
+    prevBtn = $(this)[0];
+  });
+
+});
