a272d6e983e6f9593cac83bd6cd303118072238d
Kirill Demidov <kademidov@gmail.com>
2021-10-30 01:41:57 +0300

    табы фильтры слайдер

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
+  // Меню на мобильное версии
   // let isOpen = false;
-
   $('.js-burger').on('click', function(){
 
     $('.js-main-nav').toggleClass('main-nav-open');
@@ -14,16 +14,13 @@ $(document).ready(function(){
 
     // $('.js-main-nav').addClass('main-nav-open');
     // isOpen = true;
-
   });
 
 
 
+  // Аккордион страница вопросов и ответов
   let prevBtn;
-
   $('.js-accordion-btn').on('click', function() {
-    console.log($(this));
-
 
     if (prevBtn === $(this)[0]) {
       $(this).next().slideToggle();
@@ -34,5 +31,57 @@ $(document).ready(function(){
     $(this).next().slideDown();
     prevBtn = $(this)[0];
   });
+
+
+
+  // Табы на странице контактов
+  $('.tabs-link').on('click', function(e) {
+    e.preventDefault();
+
+    let index = $(this).index('.tabs-link');
+
+    $('.tabs-link').removeClass('active');
+    $(this).addClass('active');
+
+    $('.contacts-content').removeClass('active');
+    $('.contacts-content').eq(index).addClass('active');
+
+  });
+
+  // Фильтр на странице портфолио
+  $('.filter-link').on('click', function(e) {
+    e.preventDefault();
+
+    let linkType = $(this).data('type');
+    console.log(linkType);
+
+    $('.filter-link').removeClass('active');
+    $(this).addClass('active');
+
+    if (linkType === 'all') {
+      $('.portfolio-item').show();
+      return;
+    }
+
+    $('.portfolio-item').each(function(){
+      let itemType = $(this).data('type');
+
+      if (linkType === itemType) {
+        $(this).show();
+        return;
+      }
+
+      $(this).hide();
+    });
+
+  });
+
+
+  // Слайдер
+  // Проверяем есть ли разметка для слайдера на странице
+  if ( $('.slides-list').length ) {
+    $('.slides-list').slick();
+  }
+
 
 });
